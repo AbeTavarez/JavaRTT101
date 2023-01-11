60c85af3226e7199c846c0893f861138ec3d9329
AbeTavarez <abeincwireless@gmail.com>
2023-01-10 23:34:03 -0500

    section2

---
+++ section2/Variables.java
@@ -0,0 +1,11 @@
+package section2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int passengers = 6;
+        System.out.println(passengers);
+
+        int busTickets = 6;
+        System.out.println(busTickets);
+    }
+}
