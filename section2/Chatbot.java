b41da60270a464fb066bc6c1ac95b38a26800973
AbeTavarez <abeincwireless@gmail.com>
2023-01-11 14:50:21 -0500

    javabot

--- section2/Chatbot.java
+++ section2/Chatbot.java
@@ -12,17 +12,17 @@ public class Chatbot {
         System.out.println("\nHi " +  name + "!" +" I'm Javabot. Where are you from?");
         String home = scanner.nextLine();
                         
-        System.out.println("\nI hear it's beautiful at " + home + "!" + " I'm from a place called Oracle");
+        System.out.println("\nI hear it's beautiful at " + home + "! I'm from a place called Oracle");
         System.out.println("How old are you?");
         int age = scanner.nextInt();
 
-        System.out.println("\nSo you're " + age + "," + " cool! I'm 400 years old.");
+        System.out.println("\nSo you're " + age + ", cool! I'm 400 years old.");
         System.out.println("This means I'm " + (400/age) + " times older than you.");
         System.out.println("Enough about me. What's your favorite language? (just don't say Python)");
-        scanner.nextLine();
+        scanner.nextLine(); // fixes the nextLine trap (nextInt before nextLine)
         String language = scanner.nextLine();
 
-        System.out.println("\n" + language + "," + " that's great! Nice chatting with you" + name + '.' + "I have to log off now. See ya!");
+        System.out.println("\n" + language + ", that's great! Nice chatting with you" + name + ". I have to log off now. See ya!");
         
         scanner.close();
 
